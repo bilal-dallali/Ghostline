1cd52faa4e5ccf553696845ae8755292f7661993
bilal-dallali <dallali-bilal@hotmail.fr>
2025-03-30 16:30:23 +0200

    Initial Commit

---
+++ Ghostline/GhostlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostlineApp.swift
+//  Ghostline
+//
+//  Created by Bilal Dallali on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GhostlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
