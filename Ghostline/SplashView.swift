a701e6d38c50ee982a57305491e449c0f035ad4a
bilal-dallali <dallali-bilal@hotmail.fr>
2025-05-24 09:51:00 +0200

    button

--- Ghostline/SplashView.swift
+++ Ghostline/SplashView.swift
@@ -10,24 +10,19 @@ import SwiftUI
 struct SplashView: View {
     var body: some View {
         VStack {
-//            Button {
-//                //
-//            } label: {
-//                Text("Get Started")
-//                    .foregroundStyle(Color("OtherWhite"))
-//                    .font(.custom("Urbanist-Bold", size: 16))
-//                    .frame(maxWidth: .infinity)
-//                    .frame(height: 58)
-//                    .background {
-//                        GradientBlue()
-//                    }
-//                    .clipShape(RoundedRectangle(cornerRadius: .infinity))
-//                    .shadow(color: Color(red: 36/255, green: 107/255, blue: 253/255, opacity: 0.25), radius: 12, x: 4, y: 8)
-//            }
-            VStack {
-                LazyHGrid(rows: []) {
-                    Circle()
-                }
+            Button {
+                //
+            } label: {
+                Text("Get Started")
+                    .foregroundStyle(Color("OtherWhite"))
+                    .font(.custom("Urbanist-Bold", size: 16))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 58)
+                    .background {
+                        GradientBlue()
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: .infinity))
+                    .shadow(color: Color(red: 36/255, green: 107/255, blue: 253/255, opacity: 0.25), radius: 12, x: 4, y: 8)
             }
         }
     }
